359a4ba1ec3ee9fd0bf829ea179a5404253f206d
barry <barrymwong@yahoo.com>
2013-05-24 11:08:24 -0700

    first commit. still investigating.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,46 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But in stead we're going to implement it from scratch:
+// But instead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  // You should use document.body, element.childNodes, and element.classList
+
+  var arr = [],
+      cn = document.body.childNodes,
+      //cn = document.body.getElementsByTagName('*'),
+      re = new RegExp(className,'i'),
+      i, item;
+
+/*
+  for(i=0; i<cn.length; i++){
+    if(re.test(cn[i].className) == true){
+      arr.push(cn[i]);
+      item = cn[i];
+      arr[arr.length] = item;
+
+
+    }
+  }
+*/
+
+
+      [].forEach.call( cn, function(el) {
+        if(re.test(el.className) == true){
+         //console.log(el);
+         arr[arr.length] = el;
+        }
+      });
+
+
+
+
+
+
+
+  console.log(document.getElementsByClassName(className)); // is HTMLCollection??
+  console.log(arr);
+  console.log('-----------');
+  return arr;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,41 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+
+
+	if( typeof obj === 'number' || typeof obj === 'boolean' ){
+		obj = obj.toString();
+	} else if ( obj === null ) {
+		obj = String(obj);
+	} else if ( typeof obj === 'string' ) {
+		obj = '"' + obj + '"';
+	} else if ( obj instanceof Array ) {
+		obj = '[' + obj.join() + ']';
+	} else if ( obj instanceof Object ) {
+		obj = obj.join();
+	}
+
+
+
+/*
+	// recursion
+	var rec = function(arr) {
+		console.log( 'before: ', arr );
+		console.log( 'now: ', arr.pop() );
+		if(arr.length) {
+			rec(arr);
+		} else {
+			console.log('done.');
+		}
+	};
+	rec(validObjects);
+*/
+
+
+
+	return obj;
+
 };
+
+
+
